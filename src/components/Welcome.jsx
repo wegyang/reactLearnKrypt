c54ac8dff10acf361c44aa3edc7d5a4b6c38906b
SKY-20241210CLI\Young <guangyangwei@gmail.com>
2025-02-18 09:14:52 +0800

    去掉发送代码

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -21,17 +21,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-  const { currentAccount, connectWallet, handleChange, sendTransaction, formData, isLoading } = useContext(TransactionContext);
-
-  const handleSubmit = (e) => {
-    const { addressTo, amount, keyword, message } = formData;
-
-    e.preventDefault();
-
-    if (!addressTo || !amount || !keyword || !message) return;
-
-    sendTransaction();
-  };
+  const { currentAccount, connectWallet } = useContext(TransactionContext);
 
   return (
     <div className="flex w-full justify-center items-center">
@@ -74,8 +64,8 @@ const Welcome = () => {
           </div>
         </div>
 
-        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
-          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-20 mt-20">
+          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-50 sm:w-80 w-full my-8 eth-card .white-glassmorphism ">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
@@ -92,26 +82,6 @@ const Welcome = () => {
                 </p>
               </div>
             </div>
-          </div>
-          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-            <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
-            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
-            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
-            <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
-
-            <div className="h-[1px] w-full bg-gray-400 my-2" />
-
-            {isLoading
-              ? <Loader />
-              : (
-                <button
-                  type="button"
-                  onClick={handleSubmit}
-                  className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
-                >
-                  Send now
-                </button>
-              )}
           </div>
         </div>
       </div>
